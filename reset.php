86758be61a6d9e08ee6976f269de257404768378
Adheesh Burthia <adheeshburthia1234@gmail.com>
2023-11-30 21:29:31 +0400

    Sign in sign up

--- reset.php
+++ reset.php
@@ -1,90 +1,76 @@
 <?php
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
 
-	include 'includes/session.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-	if(isset($_POST['reset'])){
-		$email = $_POST['email'];
+require $_SERVER['DOCUMENT_ROOT'] . '/Ecommerce Site PHP/ecommerce/includes/mail/Exception.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/Ecommerce Site PHP/ecommerce/includes/mail/PHPMailer.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/Ecommerce Site PHP/ecommerce/includes/mail/SMTP.php';
 
-		$conn = $pdo->open();
+include 'includes/session.php';
 
-		$stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE email=:email");
-		$stmt->execute(['email'=>$email]);
-		$row = $stmt->fetch();
+if (isset($_POST['reset'])) {
+	$email = $_POST['email'];
 
-		if($row['numrows'] > 0){
-			//generate code
-			$set='123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-			$code=substr(str_shuffle($set), 0, 15);
-			try{
-				$stmt = $conn->prepare("UPDATE users SET reset_code=:code WHERE id=:id");
-				$stmt->execute(['code'=>$code, 'id'=>$row['id']]);
-				
-				$message = "
+	$conn = $pdo->open();
+
+	$stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE email=:email");
+	$stmt->execute(['email' => $email]);
+	$row = $stmt->fetch();
+
+	if ($row['numrows'] > 0) {
+		//generate code
+		$set = '123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$code = substr(str_shuffle($set), 0, 15);
+		$stmt = $conn->prepare("UPDATE users SET reset_code=:code WHERE id=:id");
+		$stmt->execute(['code' => $code, 'id' => $row['id']]);
+		$subject = 'Password Reset Code';
+		$message = "
 					<h2>Password Reset</h2>
 					<p>Your Account:</p>
-					<p>Email: ".$email."</p>
+					<p>Email: " . $email . "</p>
 					<p>Please click the link below to reset your password.</p>
-					<a href='http://localhost/ecommerce/password_reset.php?code=".$code."&user=".$row['id']."'>Reset Password</a>
+					<a href='http://localhost/DigitalMind/password_reset.php?code=" . $code . "&user=" . $row['id'] . "'>Reset Password</a>
 				";
 
-				//Load phpmailer
-	    		require 'vendor/autoload.php';
+		$mail = new PHPMailer(true);
+		//Server settings
+		$mail->isSMTP();
+		$mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+		$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+		$mail->Port = 587; // TLS only
+		$mail->SMTPSecure = 'tls'; // ssl is deprecated
+		$mail->SMTPAuth = true;
+		$mail->Username = 'musclematrixnoreply@gmail.com'; // email
+		$mail->Password = 'uqpjwvqyetbxohda'; // password
+		$mail->setFrom('system@cksoftwares.com', 'MuscleMatrix'); // From email and name
+		$mail->addAddress($email, $firstname); // to email and name
+		$mail->Subject = $subject;
+		$mail->msgHTML($message); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+		$mail->AltBody = 'HTML messaging not supported'; // If html emails is not supported by the receiver, show this body
+		// $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+		$mail->SMTPOptions = array(
+			'ssl' => array(
+				'verify_peer' => false,
+				'verify_peer_name' => false,
+				'allow_self_signed' => true
+			)
+		);
 
-	    		$mail = new PHPMailer(true);                             
-			    try {
-			        //Server settings
-			        $mail->isSMTP();                                     
-			        $mail->Host = 'smtp.gmail.com';                      
-			        $mail->SMTPAuth = true;                               
-			        $mail->Username = 'testsourcecodester@gmail.com';     
-			        $mail->Password = 'mysourcepass';                    
-			        $mail->SMTPOptions = array(
-			            'ssl' => array(
-			            'verify_peer' => false,
-			            'verify_peer_name' => false,
-			            'allow_self_signed' => true
-			            )
-			        );                         
-			        $mail->SMTPSecure = 'ssl';                           
-			        $mail->Port = 465;                                   
-
-			        $mail->setFrom('testsourcecodester@gmail.com');
-			        
-			        //Recipients
-			        $mail->addAddress($email);              
-			        $mail->addReplyTo('testsourcecodester@gmail.com');
-			       
-			        //Content
-			        $mail->isHTML(true);                                  
-			        $mail->Subject = 'ECommerce Site Password Reset';
-			        $mail->Body    = $message;
-
-			        $mail->send();
-
-			        $_SESSION['success'] = 'Password reset link sent';
-			     
-			    } 
-			    catch (Exception $e) {
-			        $_SESSION['error'] = 'Message could not be sent. Mailer Error: '.$mail->ErrorInfo;
-			    }
-			}
-			catch(PDOException $e){
-				$_SESSION['error'] = $e->getMessage();
-			}
+		if (!$mail->send()) {
+			$_SESSION['error'] = 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+			header('location: reset.php');
+		} else {
+			$_SESSION['success'] = 'Password reset link sent';
+			header('location: reset.php');
 		}
-		else{
-			$_SESSION['error'] = 'Email not found';
-		}
-
-		$pdo->close();
-
-	}
-	else{
-		$_SESSION['error'] = 'Input email associated with account';
+	} else {
+		$_SESSION['error'] = 'Email not found';
 	}
 
-	header('location: password_forgot.php');
+	$pdo->close();
+} else {
+	$_SESSION['error'] = 'Input email associated with account';
+}
 
-?>
+header('location: password_forgot.php');
--- signup.php
+++ signup.php
@@ -17,22 +17,22 @@ if (isset($_SESSION['captcha'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sign Up</title>
-    <link rel="stylesheet" type="text/css" href="dist/css/login.css" />
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <!-- Google Recaptcha -->
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Sign Up</title>
+  <link rel="stylesheet" type="text/css" href="dist/css/login.css" />
+  <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+  <!-- Google Recaptcha -->
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 
 <body>
-    <div class="container signup-container">
-        <div class="forms-container">
-            <div class="signin-signup signup">
-                <form action="register.php" method="POST" class="sign-in-form">
-                    <h2 class="title">Sign Up</h2>
-                    <?php
+  <div class="container signup-container">
+    <div class="forms-container">
+      <div class="signin-signup signup">
+        <form action="register.php" method="POST" class="sign-in-form">
+          <h2 class="title">Sign Up</h2>
+          <?php
           if (isset($_SESSION['error'])) {
             echo "
                 <div class='callout callout-danger text-center'>
@@ -51,31 +51,27 @@ if (isset($_SESSION['captcha'])) {
             unset($_SESSION['success']);
           }
           ?>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" name="firstname" placeholder="Firstname"
-                            value="<?php echo (isset($_SESSION['firstname'])) ? $_SESSION['firstname'] : '' ?>"
-                            required>
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" name="lastname" placeholder="Lastname"
-                            value="<?php echo (isset($_SESSION['lastname'])) ? $_SESSION['lastname'] : '' ?>" required>
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" name="email" placeholder="Email"
-                            value="<?php echo (isset($_SESSION['email'])) ? $_SESSION['email'] : '' ?>" required>
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" name="password" placeholder="Password" required>
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" name="repassword" placeholder="Retype password" required>
-                    </div>
-                    <?php
+          <div class="input-field">
+            <i class="fas fa-user"></i>
+            <input type="text" name="firstname" placeholder="Firstname" value="<?php echo (isset($_SESSION['firstname'])) ? $_SESSION['firstname'] : '' ?>" required>
+          </div>
+          <div class="input-field">
+            <i class="fas fa-user"></i>
+            <input type="text" name="lastname" placeholder="Lastname" value="<?php echo (isset($_SESSION['lastname'])) ? $_SESSION['lastname'] : '' ?>" required>
+          </div>
+          <div class="input-field">
+            <i class="fas fa-envelope"></i>
+            <input type="text" name="email" placeholder="Email" value="<?php echo (isset($_SESSION['email'])) ? $_SESSION['email'] : '' ?>" required>
+          </div>
+          <div class="input-field">
+            <i class="fas fa-lock"></i>
+            <input type="password" name="password" placeholder="Password" required>
+          </div>
+          <div class="input-field">
+            <i class="fas fa-lock"></i>
+            <input type="password" name="repassword" placeholder="Retype password" required>
+          </div>
+          <?php
           if (!isset($_SESSION['captcha'])) {
             echo '
                 <div class="input-recaptcha" style="width:100%;">
@@ -84,27 +80,27 @@ if (isset($_SESSION['captcha'])) {
               ';
           }
           ?>
-                    <input type="submit" value="Signup" name="signup" class="btn solid" />
+          <input type="submit" value="Signup" name="signup" class="btn solid" />
 
-                    <div class="social-media">
-                        <a href="./" class="social-icon">
-                            <i class="fa fa-home" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div class="panels-container">
-            <div class="panel left-panel">
-                <div class="content">
-                    <h3>One of us?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus natus est.</p>
-                    <a href="login.php" class="btn transparent" id="sign-up-btn">Sign In</a>
-                </div>
-                <img src="./dist/images/register.svg" class="image" alt="">
-            </div>
-        </div>
+          <div class="social-media">
+            <a href="./" class="social-icon">
+              <i class="fa fa-home" aria-hidden="true"></i>
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
+    <div class="panels-container">
+      <div class="panel left-panel">
+        <div class="content">
+          <h3>One of us?</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus natus est.</p>
+          <a href="login.php" class="btn transparent" id="sign-up-btn">Sign In</a>
+        </div>
+        <img src="./dist/images/register.svg" class="image" alt="">
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
