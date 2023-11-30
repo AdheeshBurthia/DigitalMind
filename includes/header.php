6e471141a67812fc5a14d038b2dcc5bc5210d46d
Adheesh Burthia <adheeshburthia1234@gmail.com>
2023-11-30 15:44:22 +0400

    Cart

--- includes/header.php
+++ includes/header.php
@@ -21,101 +21,88 @@
     <!-- Magnify -->
     <link rel="stylesheet" href="magnify/magnify.min.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-  	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  	<![endif]-->
-
     <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
     <!-- Paypal Express -->
     <script src="https://www.paypalobjects.com/api/checkout.js"></script>
     <!-- Google Recaptcha -->
     <script src='https://www.google.com/recaptcha/api.js'></script>
 
-    <!-- Custom CSS -->
-    <style type="text/css">
-        /* Small devices (tablets, 768px and up) */
-        @media (min-width: 768px) {
-            #navbar-search-input {
-                width: 60px;
-            }
-
-            #navbar-search-input:focus {
-                width: 100px;
-            }
-        }
-
-        /* Medium devices (desktops, 992px and up) */
-        @media (min-width: 992px) {
-            #navbar-search-input {
-                width: 150px;
-            }
-
-            #navbar-search-input:focus {
-                width: 250px;
-            }
-        }
-
-        .word-wrap {
-            overflow-wrap: break-word;
-        }
-
-        .prod-body {
-            height: 300px;
-        }
-
-        .box:hover {
-            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-        }
-
-        .register-box {
-            margin-top: 20px;
-        }
-
-        #trending {
-            list-style: none;
-            padding: 10px 5px 10px 15px;
-        }
-
-        #trending li {
-            padding-left: 1.3em;
-        }
-
-        #trending li:before {
-            content: "\f046";
-            font-family: FontAwesome;
-            display: inline-block;
-            margin-left: -1.3em;
-            width: 1.3em;
-        }
-
-        /*Magnify*/
-        .magnify>.magnify-lens {
-            width: 100px;
-            height: 100px;
-        }
-    </style>
-
-    <!--
-    - custom css link
-  -->
+    <!--- Navbar -->
     <link rel="stylesheet" href="dist/css/navbar.css">
 
-    <!-- fonts style -->
-    <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Poppins:400,600,700&display=swap" rel="stylesheet">
-    <!-- Custom styles for this template -->
+    <!--- About -->
     <link href="dist/css/aboutus.css" rel="stylesheet" />
-    <!-- responsive style -->
     <link href="dist/css/responsive.css" rel="stylesheet" />
 
-    <!--
-    - ionicon link
-  -->
+    <!--- ionicon link -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+    <!-- Custom CSS -->
+    <style type="text/css">
+    /* Small devices (tablets, 768px and up) */
+    @media (min-width: 768px) {
+        #navbar-search-input {
+            width: 60px;
+        }
+
+        #navbar-search-input:focus {
+            width: 100px;
+        }
+    }
+
+    /* Medium devices (desktops, 992px and up) */
+    @media (min-width: 992px) {
+        #navbar-search-input {
+            width: 150px;
+        }
+
+        #navbar-search-input:focus {
+            width: 250px;
+        }
+    }
+
+    .word-wrap {
+        overflow-wrap: break-word;
+    }
+
+    .prod-body {
+        height: 300px;
+    }
+
+    .box:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .register-box {
+        margin-top: 20px;
+    }
+
+    #trending {
+        list-style: none;
+        padding: 10px 5px 10px 15px;
+    }
+
+    #trending li {
+        padding-left: 1.3em;
+    }
+
+    #trending li:before {
+        content: "\f046";
+        font-family: FontAwesome;
+        display: inline-block;
+        margin-left: -1.3em;
+        width: 1.3em;
+    }
+
+    /*Magnify*/
+    .magnify>.magnify-lens {
+        width: 100px;
+        height: 100px;
+    }
+    </style>
 
 </head>
